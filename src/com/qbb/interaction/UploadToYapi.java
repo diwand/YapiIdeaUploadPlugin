b8f38e1897a21827d19260824baa125bfe5ebe80
chengsheng <chengsheng@qbb6.com>
2019-05-11 18:09:42 +0800

    支持多module

--- src/com/qbb/interaction/UploadToYapi.java
+++ src/com/qbb/interaction/UploadToYapi.java
@@ -9,6 +9,7 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiFile;
 import com.qbb.build.BuildJsonForDubbo;
 import com.qbb.build.BuildJsonForYapi;
 import com.qbb.constant.ProjectTypeConstant;
@@ -44,13 +45,33 @@ public class UploadToYapi extends AnAction {
         // 获取配置
         try {
             String projectConfig=new String(editor.getProject().getProjectFile().contentsToByteArray(),"utf-8");
-            projectToken=projectConfig.split("projectToken\">")[1].split("</")[0];
-            projectId=projectConfig.split("projectId\">")[1].split("</")[0];
-            yapiUrl=projectConfig.split("yapiUrl\">")[1].split("</")[0];
-            projectType=projectConfig.split("projectType\">")[1].split("</")[0];
-            String[] attachs=projectConfig.split("attachUploadUrl\">");
-            if(attachs.length>1){
-                attachUpload=attachs[1].split("</")[0];
+            String[] modules=projectConfig.split("moduleList\">");
+            if(modules.length>1){
+                String[] moduleList=modules[1].split("</")[0].split(",");
+                PsiFile psiFile = (PsiFile) e.getDataContext().getData(CommonDataKeys.PSI_FILE);
+                String virtualFile=psiFile.getVirtualFile().getPath();
+                for(int i=0;i<moduleList.length;i++) {
+                    if(virtualFile.contains(moduleList[i])){
+                        projectToken = projectConfig.split(moduleList[i]+"\\.Token\">")[1].split("</")[0];
+                        projectId = projectConfig.split(moduleList[i]+"\\.Id\">")[1].split("</")[0];
+                        yapiUrl = projectConfig.split(moduleList[i]+"\\.Url\">")[1].split("</")[0];
+                        projectType = projectConfig.split(moduleList[i]+"\\.Type\">")[1].split("</")[0];
+                        String[] attachs = projectConfig.split(moduleList[i]+"\\.AttachUploadUrl\">");
+                        if (attachs.length > 1) {
+                            attachUpload = attachs[1].split("</")[0];
+                        }
+                        break;
+                    }
+                }
+            }else {
+                projectToken = projectConfig.split("projectToken\">")[1].split("</")[0];
+                projectId = projectConfig.split("projectId\">")[1].split("</")[0];
+                yapiUrl = projectConfig.split("yapiUrl\">")[1].split("</")[0];
+                projectType = projectConfig.split("projectType\">")[1].split("</")[0];
+                String[] attachs = projectConfig.split("attachUploadUrl\">");
+                if (attachs.length > 1) {
+                    attachUpload = attachs[1].split("</")[0];
+                }
             }
         } catch (Exception e2){
             Notification error = notificationGroup.createNotification("get config error:"+e2.getMessage(), NotificationType.ERROR);
