5dda5437ec275585a5f6a2a034bc9a45b56e6d36
chengsheng <chengsheng@qbb6.com>
2019-09-19 20:00:07 +0800

    fix

--- src/com/qbb/build/BuildJsonForYapi.java
+++ src/com/qbb/build/BuildJsonForYapi.java
@@ -773,7 +773,7 @@ public class BuildJsonForYapi{
                         index=index+1;
                         PsiClass psiClassChild = JavaPsiFacade.getInstance(project).findClass(childType[index].split(">")[0], GlobalSearchScope.allScope(project));
                         getCollect(kv, psiClassChild.getName(), remark, psiClassChild, project, name,pName,childType,index+1);
-                    } else if(NormalTypes.isNormalType(child)){
+                    } else if(NormalTypes.isNormalType(child)|| NormalTypes.noramlTypesPackages.containsKey(child)){
                         KV kv1 = new KV();
                         PsiClass psiClassChild = JavaPsiFacade.getInstance(project).findClass(child, GlobalSearchScope.allScope(project));
                         kv1.set(KV.by("type", psiClassChild.getName()));
