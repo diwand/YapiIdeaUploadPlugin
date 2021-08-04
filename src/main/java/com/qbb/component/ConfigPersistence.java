9046b0b5c75f26b1577af9b2cea2d5e3fe6753be
xujiaji <jiajixu@qq.com>
2021-08-04 09:55:51 +0800

    fix: idea完全关闭后持久化数据丢失

--- src/main/java/com/qbb/component/ConfigPersistence.java
+++ src/main/java/com/qbb/component/ConfigPersistence.java
@@ -1,13 +1,15 @@
 package com.qbb.component;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.intellij.openapi.components.PersistentStateComponent;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.components.State;
 import com.intellij.openapi.components.Storage;
+import com.intellij.util.xmlb.XmlSerializerUtil;
 import com.qbb.dto.ConfigDTO;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
 import java.util.List;
 
 /**
@@ -18,32 +20,34 @@ import java.util.List;
  * @date 2020/12/25
  */
 @State(name = "yapiUploads", storages = {@Storage(value = "$APP_CONFIG$/yapiUploads.xml")})
-public class ConfigPersistence implements PersistentStateComponent<List<ConfigDTO>> {
+public class ConfigPersistence implements PersistentStateComponent<ConfigPersistence> {
 
 
     public List<ConfigDTO> getConfigs() {
-        return configs;
+        if (stateValue == null) {
+            return null;
+        }
+        return new Gson().fromJson(stateValue, new TypeToken<List<ConfigDTO>>(){}.getType());
     }
 
     public void setConfigs(List<ConfigDTO> configs) {
-        this.configs = configs;
+        stateValue = new Gson().toJson(configs);
     }
-
-    private List<ConfigDTO> configs;
 
     public static ConfigPersistence getInstance(){
         return ServiceManager.getService(ConfigPersistence.class);
     }
 
+    public String stateValue;
 
     @Nullable
     @Override
-    public List<ConfigDTO> getState() {
-        return this.configs;
+    public ConfigPersistence getState() {
+        return this;
     }
 
     @Override
-    public void loadState(@NotNull List<ConfigDTO> element) {
-        this.configs = element;
+    public void loadState(@NotNull ConfigPersistence state) {
+        XmlSerializerUtil.copyBean(state, this);
     }
 }
